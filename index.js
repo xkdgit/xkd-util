34491bc3102805c75bec5e2cb62e9e0f209b75bf
dtboy <936776640@qq.com>
2018-08-24 00:05:03 +0800

    submit change

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const findOne = (arr, matcher) => {
-    if (!arr ) return null
+const find = (arr, matcher) => {
     for (let i = 0; i < arr.length; i++) {
         const ele = arr[i];
         if (matcher(ele)) {
@@ -9,17 +8,6 @@ const findOne = (arr, matcher) => {
     return null
 }
 
-const removeOne = (arr, matcher) => {
-    if (!arr) return null
-    for (let i = 0; i < arr.length; i++) {
-        const ele = arr[i];
-        if (matcher(ele)) {
-            arr.splice(i, 1)
-        }
-    }
-}
-
 module.exports = {
-    find: find,
-    remove: remove
+    find: find
 }
